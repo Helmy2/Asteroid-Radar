953cf774d9116e145c1747ab5f27507198320f4c
Helmy <mohamed0132525715@gmail.com>
2022-07-31 00:51:17 +0200

    add Accessibility to images

--- app/src/main/java/com/example/asteroidradar/util/Constants.kt
+++ app/src/main/java/com/example/asteroidradar/util/Constants.kt
@@ -1,8 +1,9 @@
 package com.example.asteroidradar.util
 
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    val API_KEY: String = System.getenv("API_KEY")!!.toString()
+    const val API_KEY: String = ""
 }
